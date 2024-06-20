c0fa176d6def68ca369c7efb1c4b1ba5109f39f3
Dafa Zakhulhaq <dafa27890@gmail.com>
2024-06-20 07:24:48 +0700

    fix: layout offside issue

--- testProject/App.js
+++ testProject/App.js
@@ -1,19 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, ScrollView, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://www.omdbapi.com/?s=star&apikey=your_api_key')  // Replace 'your_api_key' with the actual key from OMDb API
-      .then(response => response.json())
-      .then(data => {
+    fetch("http://www.omdbapi.com/?s=star&apikey=33312c01") // Replace 'your_api_key' with the actual key from OMDb API
+      .then((response) => response.json())
+      .then((data) => {
         setMovies(data.Search);
         setLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
+        console.log(error, "err");
         setLoading(false);
       });
   }, []);
@@ -28,7 +37,7 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Film Searcher</Text>
       <ScrollView horizontal style={styles.scrollView}>
         {movies.map((movie, index) => (
@@ -39,39 +48,39 @@ export default function App() {
           </TouchableOpacity>
         ))}
       </ScrollView>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   scrollView: {
-    width: '100%',
+    width: "100%",
     height: 200,
   },
   movieContainer: {
     width: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     margin: 10,
     padding: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     elevation: 3,
   },
   movieTitle: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
